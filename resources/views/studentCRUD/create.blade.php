35e7c6cf4cec935c3a245a780ebcb7f5701fb44c
yudichandra <70194796+yudichandra@users.noreply.github.com>
2021-10-13 23:53:53 +0700

    master

--- resources/views/studentCRUD/create.blade.php
+++ resources/views/studentCRUD/create.blade.php
@@ -29,37 +29,37 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Student first Name:</strong>
-                <input type="text" name="nama_depan" class="form-control" placeholder="Faculty Name">
+                <input type="text" name="nama_depan" class="form-control" placeholder="First name">
 </div>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Student last Name:</strong>
-                <input type="text" name="nama_belakang" class="form-control" placeholder="Faculty Name">
+                <input type="text" name="nama_belakang" class="form-control" placeholder="Last Name">
 </div>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Student Email:</strong>
-                <input type="text" name="email" class="form-control" placeholder="Faculty Name">
+                <input type="text" name="email" class="form-control" placeholder="Your Email">
 </div>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Student Phone:</strong>
-                <input type="text" name="no_telp" class="form-control" placeholder="Faculty Name">
+                <input type="text" name="no_telp" class="form-control" placeholder="Phone Number">
 </div>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Student Birthplace:</strong>
-                <input type="text" name="tempat_lahir" class="form-control" placeholder="Faculty Name">
+                <input type="text" name="tempat_lahir" class="form-control" placeholder="Birthplace">
 </div>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Student Birthdate:</strong>
-                <input type="text" name="tanggal_lahir" class="form-control" placeholder="Faculty Name">
+                <input type="text" name="tanggal_lahir" class="form-control" placeholder="Birthdate">
 </div>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-12 mt-5 tect-center">
